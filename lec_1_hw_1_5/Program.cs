e687aeae1803c74545f647d70f1665eb4287d11b
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-14 20:28:12 +0700

    Add project files.

---
+++ lec_1_hw_1_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lec_1_hw_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
